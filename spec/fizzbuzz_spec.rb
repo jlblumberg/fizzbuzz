d906b44f615fee93ab7ff787f7bb8aa47ddbdd75
Josh Blumberg <jlblumberg@gmail.com>
2020-01-08 15:50:51 +0000

    works for all test cases

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,5 +6,10 @@ describe "fizzbuzz" do
   it 'returns "buzz" when passed 5' do
     expect(5.fizzbuzz).to eq 'buzz'
   end
-
+  it 'returns "fizzbuzz" when called on a number that is divisible by 3 and 5' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+  it "returns #{self} when called on a number not divisible by 3 or 5" do
+    expect(17.fizzbuzz).to eq 17
+  end
 end
